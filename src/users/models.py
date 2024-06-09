d3d8bd305809b3f628774d09cb5aa9f54fb6943e
nastena <anastasiasavenok2828128@gmail.com>
2024-06-09 23:01:11 +0300

    Added CustomUser

--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from . import managers
+
+
+class UserType(models.TextChoices):
+    EMPLOYEE = 'employee'
+    CUSTOMER = 'customer'
+
+
+class CustomUser(AbstractUser):
+    username = None
+    name = models.CharField(_("Name of User"), max_length=255)
+    email = models.EmailField(_('email address'), unique=True)
+    phone = models.CharField(_('phone number'), max_length=13, unique=True)
+    user_type = models.CharField(max_length=20, choices=UserType.choices, default=UserType.EMPLOYEE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = managers.CustomUserManager()
+
+    def __str__(self):
+        return self.email
