0883e5d4d856ea18530ef96e3af9c1efdd859710
nastena <anastasiasavenok2828128@gmail.com>
2024-06-09 20:45:51 +0300

    Renamed project folder

--- config/settings.py
+++ config/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'customJira.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'customJira.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
